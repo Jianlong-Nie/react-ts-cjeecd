53d07956d151254b6f56681606e4be88a673499c
zhuchuanwu <1317272927@qq.com>
2021-06-28 13:07:03 +0800

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useCallback } from 'react';
 import { render } from 'react-dom';
 import CoverImageView from './CoverImageView';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -6,10 +6,20 @@ import { faSync } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './style.css';
 
-const error = true;
-
 const App = () => {
-  
+  const [showError, setShowError] = useState<boolean>(true);
+  const [showLoading, setshowLoading] = useState<boolean>(false);
+  const onHideError = useCallback(() => {
+    setShowError(false);
+    setshowLoading(true);
+    setTimeout(() => {
+      setshowLoading(false);
+    }, 600);
+  }, []);
+  const onShowError = useCallback(() => {
+    setShowError(true);
+    setshowLoading(false);
+  }, []);
   return (
     <div
       style={{
@@ -23,7 +33,7 @@ const App = () => {
           'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg'
         }
       />
-      {error ? (
+      {showError && (
         <div
           style={{
             display: 'flex',
@@ -34,13 +44,20 @@ const App = () => {
           <p style={{ paddingLeft: 16, color: '#d8000c' }}>
             {'Unexpected error'}
           </p>
-          <FontAwesomeIcon icon={faSync} style={{ paddingLeft: 16 }} />
+          <FontAwesomeIcon
+            icon={faSync}
+            style={{ paddingLeft: 16 }}
+            onClick={onHideError}
+          />
         </div>
-      ) : (
+      )}
+      {showLoading && <p style={{ paddingLeft: 16 }}>Loading……</p>}
+      {!showError && !showLoading && (
         <div
           style={{
             marginLeft: 32,
             marginRight: 32,
+            marginTop: -100,
             backgroundColor: '#fafafa',
             borderRadius: 12,
             border: '1px solid #c8c8c8',
@@ -48,6 +65,7 @@ const App = () => {
             display: 'flex',
             flexDirection: 'column'
           }}
+          onClick={onShowError}
         >
           <h2>{'Card Title'}</h2>
           <p>{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`}</p>
